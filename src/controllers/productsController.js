be581ae50514159fc9b5981475ae537f5a884af1
Dmytro Serhiienko <conneciones@gmail.com>
2026-05-09 16:22:39 +0300

    added routes

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,6 +1,54 @@
+import createHttpError from 'http-errors';
 import { Product } from '../models/product.js';
 
+// ALL GET
 export const getProducts = async (req, res) => {
   const products = await Product.find();
   res.status(200).json(products);
+};
+
+// ID
+export const getProductById = async (req, res) => {
+  const { productId } = req.params;
+  const product = await Product.findById(productId);
+
+  if (!product) {
+    throw createHttpError(404, 'Product not found');
+  }
+
+  res.status(200).json(product);
+};
+
+// POST
+export const createProduct = async (req, res) => {
+  const product = await Product.create(req.body);
+  res.status(201).json(product);
+};
+
+// PATCH
+export const updateProductById = async (req, res) => {
+  const { productId } = req.params;
+
+  const product = await Product.findOneAndUpdate({ _id: productId }, req.body, {
+    returnDocument: 'after',
+  });
+
+  if (!product) {
+    throw createHttpError(404, 'Product not found');
+  }
+
+  res.status(200).json(product);
+};
+
+// DELETE
+export const deleteProduct = async (req, res) => {
+  const { productId } = req.params;
+
+  const product = await Product.findOneAndDelete({ _id: productId });
+
+  if (!product) {
+    throw createHttpError(404, 'Product not found');
+  }
+
+  res.status(200).json(product);
 };
--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -1,8 +1,26 @@
 import { Router } from 'express';
-import { getProducts } from '../controllers/productsController.js';
+import {
+  getProducts,
+  getProductById,
+  createProduct,
+  updateProductById,
+  deleteProduct,
+} from '../controllers/productsController.js';
 
 const router = Router();
 
 router.get('/products', getProducts);
+
+// byID
+router.get('/products/:productId', getProductById);
+
+// POST
+router.post('/products', createProduct);
+
+// PATCH
+router.patch('/products/:productId', updateProductById);
+
+// DELETE
+router.delete('/products/:productId', deleteProduct);
 
 export default router;
