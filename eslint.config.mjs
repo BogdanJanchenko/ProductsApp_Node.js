d8918695fb7b11c8be39c3a160733d9246bbc8f8
Bodya Janchenko <robotosoid2000@gmail.com>
2026-05-09 15:38:23 +0300

    First blended

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,5 +11,10 @@ export default defineConfig([
   {
     files: ['**/*.{js,mjs,cjs}'],
     languageOptions: { globals: globals.node },
+    rules: {
+      semi: 'error',
+      'no-unused-vars': ['error', { args: 'none' }],
+      'no-undef': 'error',
+    },
   },
 ]);
--- src/server.js
+++ src/server.js
@@ -3,12 +3,23 @@ import 'dotenv/config';
 import cors from 'cors';
 
 import { connectMongoDB } from './db/connectMongoDB.js';
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
+import productsRoutes from './routes/productsRoutes.js';
 
 const app = express();
-const PORT = process.env.PORT ?? 3030;
+const PORT = process.env.PORT ?? 3000;
+
+app.use(logger);
 
 app.use(express.json());
 app.use(cors());
+
+app.use(productsRoutes);
+
+app.use(notFoundHandler);
+app.use(errorHandler);
 
 await connectMongoDB();
 app.listen(PORT, () => {
